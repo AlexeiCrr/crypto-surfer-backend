ea53f16cc0889d91c0b86e656ccd1e5c9a94f96d
alexc <alexei.crudu@pixieset.com>
2022-11-20 22:13:55 +0200

    added exceptions to cors origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+    ],
     methods: ['GET', 'POST'],
     credentials: true,
   });
