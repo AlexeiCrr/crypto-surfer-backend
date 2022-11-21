fcf2e476a1810b831ab74438e914b83fa134d5b6
alexc <alexei.crudu@pixieset.com>
2022-11-21 11:15:22 +0200

    fixed url

--- src/crypto/crypto.service.ts
+++ src/crypto/crypto.service.ts
@@ -6,25 +6,29 @@ import { CoinDto } from 'src/DTOs/crypto.dto';
 @Injectable()
 export class CryptoService {
   constructor(private readonly httpService: HttpService) {}
+
   getListings(params): Observable<CoinDto> {
     const { currency, page } = params;
-    const res = this.httpService
-      .get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&sparkline=false`,
-      )
-      .pipe(
-        map((response) => response.data),
-        map((data) =>
-          data.map((coin) => ({
-            id: coin.id,
-            name: coin.name,
-            image: coin.image,
-            currentPrice: coin.current_price,
-            circulatingSupply: coin.circulating_supply,
-            totalSupply: coin.total_supply,
-          })),
-        ),
-      );
+    const url = new URL('https://api.coingecko.com/api/v3/coins/markets');
+    url.searchParams.set('page', page);
+    url.searchParams.set('vs_currency', currency);
+    url.searchParams.set('order', 'market_cap_desc');
+    url.searchParams.set('per_page', '10');
+    url.searchParams.set('sparkline', 'false');
+
+    const res = this.httpService.get(url.toString()).pipe(
+      map((response) => response.data),
+      map((data) =>
+        data.map((coin) => ({
+          id: coin.id,
+          name: coin.name,
+          image: coin.image,
+          currentPrice: coin.current_price,
+          circulatingSupply: coin.circulating_supply,
+          totalSupply: coin.total_supply,
+        })),
+      ),
+    );
     return res;
   }
 }
